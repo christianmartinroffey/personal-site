248f838db32064e001febe89a0a4760fd0068663
christianmartinroffey <c.martinroffey@gmail.com>
2022-08-26 17:30:02 +0000

    moved darkmode functionality into flux

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,3 +1,5 @@
+import anime from 'animejs/lib/anime.es.js';
+
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
@@ -51,21 +53,13 @@ const getState = ({ getStore, getActions, setStore }) => {
 				setStore({ demo: demo });
 			},
 
-			changeToggleValueToTrue: () =>{
-				
-				setStore({toggle: true});
-				setStore({darkModeText: "Dark Mode"});
-				
-					
-				},
-
-			changeToggleValueToFalse: () =>{
-				setStore({toggle: false});
-				setStore({darkModeText: "Light Mode"});
-				},
+			// this section is for the darkmode functionality - just need to add id of dark-page to any page
 				
 			turnOnDarkMode: () =>{
-				console.log("button clicked");
+				const moonPath = "M 27.5 0 C 34.791 0 41.79 2.899 46.945 8.055 C 52.101 13.21 55 20.209 55 27.5 C 55 34.791 52.101 41.79 46.945 46.945 C 41.79 52.101 34.791 55 27.5 55 C 20.209 55 13.21 52.101 8.055 46.945 C 2.899 41.79 0 34.791 0 27.5 C 0 20.209 2.899 13.21 8.055 8.055 C 13.21 2.899 20.209 0 27.5 0 Z";
+				const sunPath = "M 27.5 0 C 34.791 0 41.79 2.899 46.945 8.055 C 33.991 9.89 26.93 20.209 26.93 27.5 C 26.93 34.791 33.689 45.261 46.945 46.945 C 41.79 52.101 34.791 55 27.5 55 C 20.209 55 13.21 52.101 8.055 46.945 C 2.899 41.79 0 34.791 0 27.5 C 0 20.209 2.899 13.21 8.055 8.055 C 13.21 2.899 20.209 0 27.5 0 Z";
+				const store = getStore()
+				console.log("button clicked from flux");
 				const timeline = anime.timeline({
 					duration : 750,
 					easing : "easeOutExpo"
@@ -74,22 +68,24 @@ const getState = ({ getStore, getActions, setStore }) => {
 				timeline
 				.add({
 					targets:".moon",
-					d:[{value: toggle ? moonPath: sunPath}] //moonPath ->sunpath
+					d:[{value: store.toggle ? moonPath: sunPath}] //moonPath ->sunpath
 				})
 				.add({
 					targets:'#dark_mode',
-					rotate : toggle? 0 : 320
+					rotate : store.toggle? 0 : 320
 				},"-=350")
 				.add({
 					targets: "#dark-page",
-					backgroundColor: toggle ? 'rgba(255,255,255, 0.9)' : 'rgba(22,22,22, 0.9)',
-					color: toggle ? 'rgba(22,22,22, 0.9)' : 'rgba(255,255,255, 0.9)'
+					backgroundColor: store.toggle ? 'rgba(255,255,255, 0.9)' : 'rgba(22,22,22, 0.9)',
+					color: store.toggle ? 'rgba(22,22,22, 0.9)' : 'rgba(255,255,255, 0.9)'
 				},"-=700");
 				
-				if(!toggle){
-					actions.changeToggleValueToTrue();
+				if(!store.toggle){
+					setStore({toggle: true});
+					setStore({darkModeText: "Dark Mode"});
 				}else{
-					actions.changeToggleValueToFalse();
+					setStore({toggle: false});
+					setStore({darkModeText: "Light Mode"});
 				}   
 
 			},
