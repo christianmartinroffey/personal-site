31c3c0864e9fe503e73b9c9f9c0c03607af471dd
christianmartinroffey <c.martinroffey@gmail.com>
2022-08-31 17:09:13 +0000

    annotations

--- src/front/js/component/BackToTop.js
+++ src/front/js/component/BackToTop.js
@@ -1,24 +1,13 @@
-import React, { useContext, useState, useEffect} from "react";
+import React from 'react'
 
 function BackToTop() {
 
-  //new image on scroll
-  const [scrolled, setScrolled] = useState();
-  const initScrollBehaviour = () => {
-    let y = window.scrollY;
-    setScrolled(y);
-    console.log(y, "scrolled");
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", () => initScrollBehaviour());
-  }, []);
-
+  
   return (
     <div id="bottom-right" className='container d-flex'>
         <div className='row'>
         <h2 className='col-6'>C.</h2>
-       {scrolled > 400 ?  <a href="#" className='col-6'>BACK TO TOP</a> : "" }
+        <a href="#" className='col-6'>BACK TO TOP</a>
         </div>
     </div>
   )
--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,8 +1,36 @@
-import React, { Component } from "react";
+import React, { useContext, useState, useEffect} from "react";
 import BackToTop from "../component/BackToTop";
 import "../../styles/home.css"
 
 export const Footer = () => (
+
+	/////// MAKE FOOTER STICKY AT X px SCROLLED ///////
+ //new image on scroll
+//     const [scrolled, setScrolled] = useState();
+//     const initScrollBehaviour = () => {
+//       let y = window.scrollY;
+//       setScrolled(y);
+//       console.log(y, "scrolled");
+//     };
+  
+//     useEffect(() => {
+//       window.addEventListener("scroll", () => initScrollBehaviour());
+//     }, []);
+  
+// if (scrolled == 300 ){
+//     const fetchRequest = async () => {
+//     const data = await fetch(`https://api.unsplash.com/photos/random?client_id=${clientID}`)  
+
+//     const dataJ = await data.json();
+//     // setRes(dataJ.results);
+//     setRes(dataJ.urls.regular);
+//     console.log(dataJ.urls.regular, "raw");
+// };
+// fetchRequest();
+// };
+
+
+	
 	<footer className="container d-flex align-right">
 		<div className=" footer mt-auto py-3 ">
 		<div id='' className="">
