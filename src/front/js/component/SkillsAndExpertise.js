9adc91ff30f514f97dad60b456e35120b0c6d5be
christianmartinroffey <c.martinroffey@gmail.com>
2022-09-02 16:28:32 +0000

    accordion with bootstrap, need to open based on ID

--- src/front/js/component/SkillsAndExpertise.js
+++ src/front/js/component/SkillsAndExpertise.js
@@ -8,18 +8,24 @@ function SkillsAndExpertise() {
   var skillsOptions = [
     {
       id: 0,
-      skillName: "Skill 1",
-      status: "In Progress"
+      skillName: "Frontend",
+      languages: "Languages: HTML, CSS, Javascript, React, Bootstrap",
+      description: "More details on languages",
+      projectExamples: "Worked on X project"
     },
     {
       id: 1,
-      skillName: "Skill 2",
-      status: "In Progress"
+      skillName: "Backend",
+      languages: "In Progress",
+      description: "More details on languages",
+      projectExamples: "Worked on X project"
     },
     {
       id: 2,
       skillName: "Skill 3",
-      status: "In Progress"
+      languages: "In Progress",
+      description: "More details on languages",
+      projectExamples: "Worked on X project"
     },
     {
       id: 3,
@@ -37,34 +43,41 @@ findSkills()
 
 const [open, setOpen] = useState(false)
 
-const openClickHandler = () =>{
+const openClickHandler = (e) =>{
   setOpen(true);
+  console.log(e);
 }
 
   return (
-    <div className='container-fluid d-flex'>
-      <h2>Skills & Expertise</h2>
-      
+    <div className='container-fluid '>
+      <h2 className='p-3'>Skills & Expertise</h2>
       {
               skillsOptions.map((skillOption, index)=>{ return(
-              <div className=''>
-                <ul>
-<li>
-    <input type="checkbox" checked></input>
-    <i></i>
-    <h2>{skillOption.skillName}</h2>
-    <p>{skillOption.status}.</p>
-  </li>
-  </ul>
-  
+        <div class="accordion accordion-flush" id="accordionFlushExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingOne">
+      <button class="accordion-button collapsed"   onClick={(e) => openClickHandler(e.target.id)} type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne" id={index}>
+        {skillOption.skillName}
+      </button>
+    </h2>
+    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+      
+      <div class="accordion-body">
+        <p>{skillOption.languages}</p>
+        <p>{skillOption.description}.</p>
+        <p>{skillOption.projectExamples}.</p>
+      </div>
     </div>
-                
+  </div>
+</div>
                 )  })
             }   
-      
-          
+ 
     </div>
   )
 }
+
+
+
 
 export default SkillsAndExpertise
