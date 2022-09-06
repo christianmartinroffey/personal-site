097e0dc2545765ae9981117fa8425adaf0d25091
christianmartinroffey <c.martinroffey@gmail.com>
2022-09-06 15:29:08 +0000

    alignment for about section

--- src/front/js/component/AboutMe.js
+++ src/front/js/component/AboutMe.js
@@ -4,21 +4,25 @@ import "../../styles/customstyles.css/aboutMe.css";
 
 function AboutMe() {
   return (
-    <div className="justify-content-center  " id="dark-page">
-      <div className="container d-flex pt-5">
+    <div className="justify-content-center pt-5 " id="dark-page">
+      <div className="container center-all d-flex pt-5 pb-5">
         {/* <h1 className='ml2 '>Hi! I'm Christian.</h1>
         <h1 className='ml2 pb-1'>A Full Stack Software Engineer.</h1> */}
+        <div className="col-6">
         <h1 className="home-title">
           <span className="">Hi! I'm Christian.</span>
           <span className="">A Full Stack Software Engineer.</span>
         </h1>
+        </div>
+        <div className="col-6">
         <img
           className="mx-auto d-block img-fluid diamond-animation "
           src={profileCircle}
           style={{ height: 300 }}
         ></img>
+        </div>
       </div>
-      <h5>What you'll find in this portfolio site:</h5>
+      <h2>What you'll find in this portfolio site:</h2>
       <div className="container d-flex center-all">
         <ul>
           <li>
