a6aaf9d80664488d1851cd2a4cd4e84c74666614
christianmartinroffey <c.martinroffey@gmail.com>
2022-09-06 11:45:32 +0000

    languages icon section done

---
+++ src/front/js/component/languageIcons.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import "../../styles/home.css"
+
+function LanguageIcons() {
+  return (
+    <div>
+         <div className="container mt-4 mb-3 p-4">
+          <h2 className='pb-3'>Languages</h2>
+		  <div className=" container d-flex justify-content-center">
+          <img
+		  className="img-fluid icon-size m-4"
+            alt="Visual Studio Code"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg"
+          />
+          <img className="img-fluid m-4 icon-size"
+            alt="HTML5"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
+          />
+          <img className="img-fluid m-4 icon-size"
+            alt="CSS3"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+          />
+          <img className="img-fluid m-4 icon-size"
+            alt="JavaScript"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+          />
+          <img className="img-fluid m-4 icon-size"
+            alt="React"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+          />
+          <img className="img-fluid m-4 icon-size"
+            alt="Python"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
+          />
+          <img className="img-fluid m-4 icon-size"
+            alt="Flask"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original.svg"
+          />
+          <img className="img-fluid m-4 icon-size"
+            alt="Git"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
+          />
+
+          <img className="img-fluid m-4 icon-size"
+            alt="GitHub"
+            src="https://user-images.githubusercontent.com/3369400/139448065-39a229ba-4b06-434b-bc67-616e2ed80c8f.png"
+          />
+          <img className="img-fluid  m-4 icon-size"
+            alt="Jest"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg"
+          />
+        </div>
+		</div>
+        </div>
+  )
+}
+
+export default LanguageIcons
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,6 +8,8 @@ import UnsplashAPIIMG from "../component/UnsplashAPIIMG";
 import PreviousExperience from "../component/PreviousExperience";
 import AboutMe from "../component/AboutMe";
 import SkillsAndExpertise from "../component/SkillsAndExpertise";
+import LanguageIcons from "../component/languageIcons";
+
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
@@ -19,53 +21,7 @@ export const Home = () => {
     <div className="">
       <div id="dark-page" className="text-center">
         <AboutMe />
-        <div className="container p-1">
-          <h2>Languages</h2>
-		  <div className=" container d-flex">
-          <img
-		  className="thumbnail"
-            alt="Visual Studio Code"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg"
-          />
-          <img className="thumbnail"
-            alt="HTML5"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
-          />
-          <img
-            alt="CSS3"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
-          />
-          <img
-            alt="JavaScript"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
-          />
-          <img
-            alt="React"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
-          />
-          <img
-            alt="Python"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
-          />
-          <img
-            alt="Flask"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original.svg"
-          />
-          <img
-            alt="Git"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
-          />
-
-          <img
-            alt="GitHub"
-            src="https://user-images.githubusercontent.com/3369400/139448065-39a229ba-4b06-434b-bc67-616e2ed80c8f.png"
-          />
-          <img
-            alt="Jest"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg"
-          />
-        </div>
-		</div>
+        <LanguageIcons/>
         <SkillsAndExpertise />
         <hr></hr>
         <PreviousExperience />
