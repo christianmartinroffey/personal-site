2054d62bcdf78a1a145d796cab10607a6d9d79fa
christianmartinroffey <c.martinroffey@gmail.com>
2022-09-06 13:03:02 +0000

    styling updates to about me section

--- src/front/js/component/AboutMe.js
+++ src/front/js/component/AboutMe.js
@@ -4,11 +4,14 @@ import "../../styles/customstyles.css/aboutMe.css";
 
 function AboutMe() {
   return (
-    <div className="justify-content-center" id="dark-page">
+    <div className="justify-content-center  " id="dark-page">
         <h1 className='ml2'>Welcome to my portfolio</h1>
-        <div className='container d-flex'>
-        <img src= {profileCircle} style={{height:250}}></img>
-        <p>I've spent over 7 years in software SAAS companies learning and understanding customers' needs on various levels. 
+        <div className='container d-flex center-all'>
+        <img className="mx-auto d-block img-fluid " src= {profileCircle} style={{height:300}}></img>
+        <div className='vertical-line'>
+
+        </div>
+        <p className='pl-3'>I've spent over 7 years in software SAAS companies learning and understanding customers' needs on various levels. 
           technical, mission, business etc. 
         </p>
         </div>
--- src/front/js/component/languageIcons.js
+++ src/front/js/component/languageIcons.js
@@ -3,9 +3,9 @@ import "../../styles/home.css"
 
 function LanguageIcons() {
   return (
-    <div>
-         <div className="container mt-4 mb-3 p-4">
-          <h2 className='pb-3'>Languages</h2>
+    
+         <div className="container mt-4 mb-3 p-4" id="dark-page">
+          <h2 className='pb-3'>Languages & Tools</h2>
 		  <div className=" container d-flex justify-content-center">
           <img
 		  className="img-fluid icon-size m-4"
@@ -32,7 +32,7 @@ function LanguageIcons() {
             alt="Python"
             src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
           />
-          <img className="img-fluid m-4 icon-size"
+          <img className="img-fluid m-4 icon-size bg-white rounded-circle"
             alt="Flask"
             src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original.svg"
           />
@@ -41,7 +41,7 @@ function LanguageIcons() {
             src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
           />
 
-          <img className="img-fluid m-4 icon-size"
+          <img className="img-fluid m-4 icon-size bg-white rounded-circle"
             alt="GitHub"
             src="https://user-images.githubusercontent.com/3369400/139448065-39a229ba-4b06-434b-bc67-616e2ed80c8f.png"
           />
@@ -51,7 +51,7 @@ function LanguageIcons() {
           />
         </div>
 		</div>
-        </div>
+
   )
 }
 
