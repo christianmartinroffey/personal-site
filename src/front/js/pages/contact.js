fcd378d2f97fd1b9e3e153fad3ec59269408a7de
christianmartinroffey <c.martinroffey@gmail.com>
2022-09-13 16:37:50 +0000

    style tweaks to form

--- src/front/js/pages/contact.js
+++ src/front/js/pages/contact.js
@@ -10,17 +10,20 @@ console.log("button clicked");
 
   return (
 
+    <div className='pt-5 pb-5'>
     <div className="wrapper pt-5 pb-5"> 
 
     <form className="form">
       <div className="pageTitle title">Get In Touch </div>
-      <div className="secondaryTitle title">Add your details below so I can reach out.</div>
+      <div className="secondaryTitle title">Add your details below so I can reach out, or download my CV ...  <a className='form-dowload-link' href="">here</a></div>
       <input type="text" className="name formEntry" placeholder="Name" ></input>
       <input type="text" className="email formEntry" placeholder="Email"></input>
       <textarea className="message formEntry" placeholder="Message"></textarea>
       <button className="submit formEntry btn-primary" onClick={sendEmail}>Submit</button>
     </form>
     </div>
+</div>
+    
   )
 }
 
