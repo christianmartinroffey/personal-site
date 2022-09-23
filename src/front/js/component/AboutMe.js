d10e15912043b32848f636c3417be17521a8f4b3
christianmartinroffey <c.martinroffey@gmail.com>
2022-09-23 17:41:32 +0000

    fixed intro section formatting for mobile

--- src/front/js/component/AboutMe.js
+++ src/front/js/component/AboutMe.js
@@ -3,6 +3,7 @@ import profileCircle from "../../img/profile-circle.png";
 import "../../styles/customstyles.css/aboutMe.css";
 import LanguageIcons from "./languageIcons";
 import PreviousExperience from "./PreviousExperience";
+import IntroSection from "./introSection";
 
 function AboutMe() {
 
@@ -12,7 +13,7 @@ function AboutMe() {
 // };
 
   return (
-    <div className="justify-content-center pt-5 " id="dark-page">
+    <div className="justify-content-center pt-3 " id="dark-page">
       <div className="container center-all d-flex pt-5 pb-5">
         {/* <h1 className='ml2 '>Hi! I'm Christian.</h1>
         <h1 className='ml2 pb-1'>A Full Stack Software Engineer.</h1> */}
@@ -41,44 +42,7 @@ function AboutMe() {
           </div>
         </div>
         </a>
-       
-      <h2 className="pb-4 display-4" id="portfolio-section">A brief intro:</h2>
-      <div className="container d-flex center-all">
-        <ul>
-          <li className="display-6">Professional</li>
-          <li className="pt-4 pb-4">
-          - 7+ years web-based SAAS experience, across Enterprise & SMB. 💻 
-          </li>
-          <li className=" pb-4"> 
-          - Coding mentor for 4Geeks 🚀 
-          </li>
-          <li className=" pb-4"> 
-          - Technical Support, Account Management, Department & Global Leadership. 👨‍💻
-          </li>
-          <li className=" pb-4"> 
-          - Adept at identifying improvement opportunities and increasing operational efficiency. 💡
-          </li>
-        </ul>
-        <div className="vertical-line"></div>
-        <ul>
-        <li className="display-6 pb-4">Personal</li>
-        <li className="pl-3 pb-4">
-          - I'm a father to baby Noah. 👶
-        </li>
-        <li className="pl-3 pb-4">
-          - CrossFit athlete, coach (in my spare time) & gym afficionado. 🏋️‍♂️
-        </li>
-        <li className="pl-3 pb-4">
-          - Live in paradise 🌴 Mallorca, Spain 🌴 
-        </li>
-        <li className="pl-3 pb-4">
-          - Love a challenge and enjoying the Full Stack Developer journey. 🖥️ 
-        </li>  
-        <li className="pl-3">
-          - Fluent in 3 languages, English 🇬🇧 & Spanish 🇪🇸 native, Italian 🇮🇹 professional proficiency.
-        </li>
-        </ul>
-      </div>
+    <IntroSection/>
     </div>
   );
 }
