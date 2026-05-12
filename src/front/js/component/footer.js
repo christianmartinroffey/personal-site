3a2a4d62c8415c4fb8cfb5fdd3842cc32ec761b7
christianroffey <c.martinroffey@gmail.com>
2026-05-12 15:31:25 +0200

    tweak footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -4,7 +4,7 @@ import "../../styles/home.css";
 export const Footer = () => (
   <footer className="modern-footer">
     <div className="portfolio-container">
-      <p>Designed and built by Christian M-R.</p>
+      <p>Designed and built by Christian M-R - self-hosted on Raspberry Pi 5</p>
       <div>
         <a href="https://github.com/christianmartinroffey" target="_blank" rel="noreferrer">GitHub</a>
         <span aria-hidden="true"> · </span>
